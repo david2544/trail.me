995465b6b5a8602e1ce1e6ed122b72db3cc07fdc
david.c <david.coroian@raisin.com>
2020-09-11 14:16:47 +0200

    Fix throttle

--- app/hooks/useInfiniteScroll.ts
+++ app/hooks/useInfiniteScroll.ts
@@ -1,52 +1,55 @@
-import { useEffect } from 'react';
-import isEmpty from 'lodash/isEmpty';
-import useFetchHikesData from './useFetchHikesData';
+import { useEffect, useState } from 'react';
+import Firebase from 'firebase';
 import useScroll from './useScroll';
 
-const loadMoreData = ({
-  hikesData,
-  setHikesData,
-  fetchNewData,
-  setListPos,
-  setIsFetching,
-  setAllHikesLoaded,
-}) => {
+const fetchData = (
+  hikesData: object,
+  setHikesData: Function,
+  listPos: string,
+  setIsFetching: Function,
+  setAllHikesLoaded: Function,
+) => {
+  const ref = Firebase.database()
+    .ref('hikes')
+    .orderByKey()
+    .startAt('0')
+    .endAt(listPos)
+    .limitToLast(3);
+
+  ref.on('value', snapshot => {
+    const res = snapshot.val();
+    if (res === null) {
+      setAllHikesLoaded(true);
+    } else {
+      setHikesData({ ...hikesData, ...res });
+    }
+
+    setIsFetching(false);
+  });
+};
+
+const loadDataTrigger = ({ hikesData, setHikesData, setIsFetching, setAllHikesLoaded }) => {
   setTimeout(() => {
     const listPosition = (Number(Object.keys(hikesData).sort()[0]) - 1).toString();
-    fetchNewData(hikesData, setHikesData, listPosition, setIsFetching, setAllHikesLoaded);
-    setListPos(listPosition);
+    fetchData(hikesData, setHikesData, listPosition, setIsFetching, setAllHikesLoaded);
   }, 1000);
 };
 
 const useInfiniteScroll = () => {
-  const {
-    hikesData,
-    setHikesData,
-    fetchNewData,
-    isFetching,
-    setIsFetching,
-    setListPos,
-    setAllHikesLoaded,
-    allHikesLoaded,
-  } = useFetchHikesData();
+  const [hikesData, setHikesData] = useState({});
+  const [isFetching, setIsFetching] = useState(false);
+  const [allHikesLoaded, setAllHikesLoaded] = useState(false);
   const { scrollPosition } = useScroll();
 
-  if (
-    window.innerHeight + scrollPosition >= document.body.scrollHeight &&
-    !isEmpty(hikesData) &&
-    scrollPosition !== 0 &&
-    !isFetching
-  ) {
+  if (window.innerHeight + scrollPosition >= document.body.scrollHeight && !isFetching) {
     setIsFetching(true);
   }
 
   useEffect(() => {
     if (!isFetching) return;
-    loadMoreData({
+    loadDataTrigger({
       hikesData,
       setHikesData,
-      fetchNewData,
-      setListPos,
       setIsFetching,
       setAllHikesLoaded,
     });
